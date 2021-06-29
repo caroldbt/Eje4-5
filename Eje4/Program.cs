7907c88dde039b691b8bf27da0debcdf78bf18d8
caroldbt <carol123dbt@gmail.com>
2021-06-30 00:19:52 +0200

    Ejercicios1-5

---
+++ Eje4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Eje4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("+---------Ejercicio 4--------+");
+            int N = 1;
+            int incrementa = (int)(N + 77);
+            int decrementa = incrementa - 3;
+            int duplica = decrementa * 2;
+
+            System.Console.WriteLine("Incrementar en 77: " + incrementa);
+            System.Console.WriteLine("Decrementar en 3: " + decrementa);
+            System.Console.WriteLine("Duplicar su valor: " + duplica);
+
+        }
+    }
+}
